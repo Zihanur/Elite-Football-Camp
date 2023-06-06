5d05ce395498e558b4282752835dccf126b28d05
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-07 01:39:57 +0600

    Added Routers and Footer

---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedinIn,
+  FaTwitterSquare,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 bg-neutral text-neutral-content">
+        <div>
+          <h1 className="text-3xl font-bold text-green-400">
+            Elite Football <br />
+            <span className="text-lime-400">Camp</span>
+          </h1>
+        </div>
+        <div>
+          <span className="text-xl font-bold text-green-400">Company</span>
+          <a className="link link-hover text-base">About us</a>
+          <a className="link link-hover text-base">Contact</a>
+          <a className="link link-hover text-base">Jobs</a>
+          <a className="link link-hover text-base">Press kit</a>
+        </div>
+        <div>
+          <span className="text-xl font-bold text-green-400">Social</span>
+          <div className="flex flex-col gap-2">
+            <Link className="flex items-center gap-3 text-base">
+              <FaFacebookSquare></FaFacebookSquare>FaceBook
+            </Link>
+            <Link className="flex items-center gap-3 text-base">
+              <FaTwitterSquare></FaTwitterSquare> Twitter
+            </Link>
+            <Link className="flex items-center gap-3 text-base">
+              <FaInstagramSquare></FaInstagramSquare> Instagram
+            </Link>
+            <Link className="flex items-center gap-3 text-base">
+              <FaLinkedinIn></FaLinkedinIn> LinkedinIn
+            </Link>
+          </div>
+        </div>
+      </footer>
+      <p className="footer justify-center pb-4 bg-neutral text-neutral-content">
+        Copyright © 2023 - Elite Football Industries Ltd
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
