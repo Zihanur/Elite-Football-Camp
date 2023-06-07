e50691fc0c92c2ac55adf3f558be0883f95fbb2c
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-08 00:03:01 +0600

    Create AuthProvider and createContect

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,73 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../Firebase/firebase.config";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  const userProfileUpdate = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  useEffect(() => {
+    const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("Current User", currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubcribe();
+    };
+  }, []);
+
+  const authinfo = {
+    user,
+    loading,
+    createUser,
+    userLogin,
+    googleLogin,
+    userProfileUpdate,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authinfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
